631d4db32e83e5dacc6fc548481af69e59914ccf
Ayush Poddar <ayush_poddar@live.com>
2019-09-13 18:41:04 +0530

    setup

---
+++ server/server-api.js
@@ -0,0 +1,20 @@
+const compression = require('compression');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const debug = require('debug')('app:');
+
+app.use(compression());
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	debug(`Server listening on Port ${PORT}`);
+});
